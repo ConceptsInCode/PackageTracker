0049dfa251b4f64017395915157900b0c5be7ef0
BJ Miller <john.miller@dxydoes.com>
2015-04-26 14:40:42 -0400

    Create initial project.

---
+++ PackageTracker/PackageTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PackageTracker
+//
+//  Created by BJ on 4/26/15.
+//  Copyright (c) 2015 Concepts In Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
