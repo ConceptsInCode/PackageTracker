df4dd22b79c1714df7964d41badeeabf7d79557a
Joe Masilotti <joseph.masilotti@gmail.com>
2015-11-28 13:06:15 -0500

    Make MockPackageManager for test assertions

---
+++ PackageTracker/PackageTrackerTests/MockPackageManager.swift
@@ -0,0 +1,17 @@
+//
+//  MockPackageManager.swift
+//  PackageTracker
+//
+//  Created by Joe Masilotti on 11/28/15.
+//  Copyright © 2015 Concepts In Code. All rights reserved.
+//
+
+@testable import PackageTracker
+
+class MockPackageManager: PackageManager {
+    var lastRequestInfo: USPSRequestInfo?
+
+    func fetchPackageResults(requestInfo: USPSRequestInfo, completionHandler: (([String]) -> Void)?) {
+        lastRequestInfo = requestInfo
+    }
+}
